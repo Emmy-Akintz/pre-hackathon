965611261c33a429c7fa57b698112963b7cc9836
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-23 23:31:53 +0100

    Update

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import image from '../assets/3d-graphic-designer-showing-thumbs-up-png-1.png'
 
 function Register() {
   return (
@@ -9,6 +10,15 @@ function Register() {
         </div>
         <div className="text-purple-500 block lg:hidden p-12">
             Register
+        </div>
+        <div className="lg:flex justify-between">
+            <div className="w-[200px] m-auto lg:w-[500px] lg:m-0">
+                <img src={image} alt="" />
+            </div>
+            <div className="p-5 m-auto w-[300px]">
+                <p className="">Be part of this movement <span className="purple">..........</span></p>
+                <p className="">CREATE YOUR ACCOUNT</p>
+            </div>
         </div>
     </div>
   )
